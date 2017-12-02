c731b5ef2be9d6781b3f47e801f0bb7d422c4e94
Krista <krista.abraham@gmail.com>
2017-12-02 16:05:31 -0500

    Yelp box styling framed out

--- client/src/components/results.jsx
+++ client/src/components/results.jsx
@@ -9,32 +9,102 @@ const style = {
     margin: '0 auto',
     padding: 50,
     textAlign: 'center',
-    width: '50%',
+    width: '60%',
   },
   separator: {
     height: '400px',
+  },
+  flexContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignContent: 'space-between',
+    border: '1px solid black'
+  },
+  restaurantBox: {
+    border: '1px solid blue',
+    display: 'flex',
+    flexDirection: 'row',
+    padding: '10px 0px 10px 0px'
+
   }
 };
 
 const Results = ({ results }) => {
-  //console.log(results);
+  console.log('RESULTS to client:',results);
   return (
     <div>
       <Paper style={style.paper} zDepth={3}>
         <h2>Your Results!</h2>
         <MapsContainer results={results}/>
         <div style={style.separator}></div>  {/* this provides the buffer between the map and the results */}
-        <h1>#1</h1>                          {/* otherwise the map will overlap the results */}
-        <h2>{results[0][0].name}</h2>
-        {results[0][0].formatted_address}
+                                             {/* otherwise the map will overlap the results */}
+
+
+        <div style={style.flexContainer}>
+        <h1>#1</h1>
+        <div style={style.restaurantBox}>
+          <div>
+            <img src={results[0][0].yelpImg} style={{maxWidth: '90%'}}></img>
+          </div>
+
+          <span>
+            <h3>{results[0][0].name}</h3>
+            <p>
+            {results[0][0].formatted_address}
+            </p>
+            <a>{results[0][0].rating} Stars</a><a>{results[0][0].yelpReviewCount} Reviews</a>
+            <p>
+            {results[0][0].reviews[0]}
+            </p>
+            Read More
+            <a href={results[0][0].yelpUrl}>Check out on Yelp</a>
+            </span>
+        </div>
+
 
         <h1>#2</h1>
-        <h2>{results[1][0].name}</h2>
-        {results[1][0].formatted_address}
+        <div style={style.restaurantBox}>
+          <div>
+            <img src={results[1][0].yelpImg} style={{maxWidth: '90%'}}></img>
+          </div>
+
+          <span>
+            <h3>{results[1][0].name}</h3>
+            <p>
+            {results[1][0].formatted_address}
+            </p>
+            <a>{results[1][0].rating} Stars</a><a>{results[1][0].yelpReviewCount} Reviews</a>
+            <p>
+            {results[1][0].reviews[0]}
+            </p>
+            Read More
+            <a href={results[1][0].yelpUrl}>Check out on Yelp</a>
+            </span>
+        </div>
+
 
         <h1>#3</h1>
-        <h2>{results[2][0].name}</h2>
-        {results[2][0].formatted_address}
+        <div style={style.restaurantBox}>
+          <div>
+            <img src={results[2][0].yelpImg} style={{maxWidth: '90%'}}></img>
+          </div>
+
+          <span>
+            <h3>{results[2][0].name}</h3>
+            <p>
+            {results[2][0].formatted_address}
+            </p>
+            <a>{results[2][0].rating} Stars</a><a>{results[2][0].yelpReviewCount} Reviews</a>
+            <p>
+            {results[2][0].reviews[0]}
+            </p>
+            Read More
+            <a href={results[2][0].yelpUrl}>Check out on Yelp</a>
+            </span>
+        </div>
+
+      </div>
+
       </Paper>
     </div>
   );
